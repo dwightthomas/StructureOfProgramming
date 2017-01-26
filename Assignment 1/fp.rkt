a705ea922be2d4decf372fa3596a87e996aef2fb
Dwight Thomas <dgit200@hotmail.com>
2017-01-26 14:35:28 -0500

    question 2 finished with testing and comments

--- Assignment 1/fp.rkt
+++ Assignment 1/fp.rkt
@@ -11,12 +11,15 @@
    )
  )
 
+;;This function is used to get the some of numbers in the list
+;;excluding those that are in sublist however, so only the numbers
+;;in the main list
 (define (sum-up-numbers-simple L)
-  (cond ((null? L) 0)
-        ((number? (car L))
-         (+ (car L) (sum-up-numbers-simple (cdr L)))
+  (cond ((null? L) 0) ;;this checks to see if the list is empty then return 0 if true
+        ((number? (car L)) ;;checcks to make the first character in the list is a number
+         (+ (car L) (sum-up-numbers-simple (cdr L))) ;;then adds it to our total
          )
-        (else (+ 0 (sum-up-numbers-simple (cdr L))))
+        (else (+ 0 (sum-up-numbers-simple (cdr L)))) ;;else it adds zero to the total.
    )
  )
          
